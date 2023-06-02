35ced877503e371295ecfb19a371c2dab813ca74
Ch1nbenNaBha1 <md.abid.hasan.mitul@g.bracu.ac.bd>
2023-06-02 19:56:08 +0600

    Stack

---
+++ Java/Code/stack.java
@@ -0,0 +1,55 @@
+package Java.Code;
+
+public class stack {
+    private static class Node {
+        private int data;
+        private Node next;
+
+        private Node(int data) {
+            this.data = data;
+        }
+    }
+
+    private Node top;
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public int peek() {
+        return top.data;
+    }
+
+    public void push(int data) {
+        Node node = new Node(data);
+        node.next = top;
+        top = node;
+    }
+
+    public int pop() {
+        int data = top.data;
+        top = top.next;
+        return data;
+    }
+
+    public void printList() {
+        Node current = top;
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
+    public static void main(String[] args) {
+
+        stack stack = new stack();
+     
+
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+        stack.push(60);
+
+        stack.printList();
+    }
+}
