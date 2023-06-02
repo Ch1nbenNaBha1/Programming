854fd283962a2854b03c516aaee8319b987a61bf
Ch1nbenNaBha1 <md.abid.hasan.mitul@g.bracu.ac.bd>
2023-06-02 19:35:55 +0600

    Linked

--- Java/Code/LinkedList.java
+++ Java/Code/LinkedList.java
@@ -3,4 +3,59 @@ package Java.Code;
 public class LinkedList {
     Node head;
 
+    public void append(int data) {
+        if (head == null) {
+            head = new Node(data);
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = new Node(data);
+    }
+
+    public void prepend(int data) {
+        Node newHead = new Node(data);
+        newHead.next = head;
+        head = newHead;
+    }
+
+    public void deleteWithvalue(int data) {
+        if (head == null)
+            return;
+        if (head.data == data) {
+            head = head.next;
+            return;
+        }
+
+        Node current = head;
+        while (current.next != null) {
+            if (current.next.data == data) {
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
+    }
+
+    public void printList() {
+        Node current = head;
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.append(10);
+        list.append(20);
+        list.append(30);
+        list.append(40);
+        list.prepend(5);
+        list.deleteWithvalue(20);
+        list.printList();
+    }
+
 }
