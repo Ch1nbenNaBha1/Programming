028a89827f14f6c7cdfc40c461475eb253a30e10
Ch1nbenNaBha1 <md.abid.hasan.mitul@g.bracu.ac.bd>
2023-06-12 08:48:48 +0600

    Lab 2 Finished

---
+++ CSE220 LAB/Lab2.java
@@ -0,0 +1,10 @@
+public class Lab2 {
+
+    public static void walk_zigzag(int[] floor) {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
