f3396a81f9729323c0f1ffcdc7d08819469c23ae
Ch1nbenNaBha1 <md.abid.hasan.mitul@g.bracu.ac.bd>
2023-07-28 20:20:26 +0600

    Done

--- C++/recurrsion.cpp
+++ C++/recurrsion.cpp
@@ -2,19 +2,18 @@
 
 using namespace std;
 
-void print(int arr[], int n)
+// write a program to print array using recurrsion
+void print(int arr[], int idx)
 {
-    if (n >= 0 && n <= sizeof(arr) / sizeof(arr[0]))
+    if (idx == sizeof(arr) / sizeof(arr[0]))
     {
         return;
     }
-    print(arr, n + 1);
-    cout << arr[n + 1] << " ";
+    cout << arr[idx] << endl;
+    print(arr, idx + 1);
 }
-
 int main(int argc, char const *argv[])
 {
-    int arr[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    int arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130};
     print(arr, 0);
-    return 0;
 }
--- Java/Code/Basics/recurrsion.java
+++ Java/Code/Basics/recurrsion.java
@@ -1,7 +1,37 @@
 package Java.Code.Basics;
 
-class recurrsion {
+class recursion {
+
+    public static void Print(int[] arr , int index) {
+
+        if (index >= 0 && index <= arr.length)
+        {
+            if (index == arr.length) {
+                return;
+            }
+            System.out.println(arr[index]);
+            Print(arr, index + 1);
+        }
+    }
+
+
+    public static void PrintReverse(int[] arr, int index ) {
+        if (index >= 0 && index <= arr.length){
+            if (index == arr.length) {
+                return;
+            }
+            PrintReverse(arr, index + 1);
+            System.out.println(arr[index]);
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int[] arr = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100  };
+        System.out.printf("Array Length: %d\n", arr.length);
+        Print(arr, 0);
+        System.out.println("===============================");
+        PrintReverse(arr, 0);
     }
 }
+
+
+
